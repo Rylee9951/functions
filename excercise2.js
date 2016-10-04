a935ac7cc60e29b162a3c7d4e35d1e2c9e781fa9
Ryan Lee <RyanLee@Ryans-MacBook-Pro.local>
2016-10-04 16:02:38 -0700

    almost

--- excercise2.js
+++ excercise2.js
@@ -13,7 +13,7 @@ function maxOfThree(x,y,z){
 console.log(maxOfThree(2,8,5))
 
 function isVowel(char){
-	 var list = ['a', 'e','i','o','u']
+	 var list = ['a', 'e','i','o','u','y']
 	 
 	if(list .indexOf(char) == -1){
 		return false
@@ -22,3 +22,22 @@ function isVowel(char){
 	}
 }
 console.log(isVowel('e'))
+
+function Rovarspraket(phrase){
+	var newWord = "";
+	 
+	 for(var i = 0; i < phrase.length; i += 1) {
+	 	
+	 	if(isVowel(phrase[i])){
+         newWord += phrase[i];
+	 	} else  {
+	 	newWord += phrase[i] + "o" + phrase[i];
+       
+	 }
+	 
+	 
+	}
+	return newWord;
+}
+
+console.log(Rovarspraket("ryan"))
